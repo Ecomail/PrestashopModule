df059f0191fceb97215c2e6080edfd20b083231f
Ecomail <info@ecomail.cz>
2014-09-01 08:40:48 +0200

    Update ecomail.php

--- ecomail.php
+++ ecomail.php
@@ -5,7 +5,7 @@ if (!defined('_PS_VERSION_'))
   
 /**
  * 
- * @author Ecomail.cz s.r.o.
+ * @author Christophe Willemsen
  */
 class Ecomail extends Module {
 	
@@ -43,13 +43,15 @@ class Ecomail extends Module {
 
 	    if (Tools::isSubmit('submit'.$this->name))
 	    {
-	        $my_module_name = Tools::getValue('ecomail_default_list');
-	        if (!$my_module_name
-	          || empty($my_module_name))
+	        $ecomail_list_id = Tools::getValue('ecomail_list_id');
+			$ecomail_api_key = Tools::getValue('ecomail_api_key');
+	        if (!$ecomail_list_id
+	          || empty($ecomail_list_id))
 	            $output .= $this->displayError($this->l('Invalid Configuration value'));
 	        else
 	        {
-	            Configuration::updateValue('ecomail_default_list', $my_module_name);
+	            Configuration::updateValue('ecomail_list_id', $ecomail_list_id);
+				Configuration::updateValue('ecomail_api_key', $ecomail_api_key);
 	            $output .= $this->displayConfirmation($this->l('Settings updated'));
 	        }
 	    }
@@ -61,6 +63,18 @@ class Ecomail extends Module {
 	    // Get default language
 	    $default_lang = (int)Configuration::get('PS_LANG_DEFAULT');
 
+		$options = array();
+		
+		if(Configuration::get('ecomail_api_key')){
+			$lists = json_decode($this->callAPI(Configuration::get('ecomail_api_key'), 'GET', 'lists'));
+			foreach($lists->_embedded->lists as $list){
+				$options[] = array(
+					'id_option' => $list->id,
+				    'name' => $list->name
+				);
+			}
+		}
+		
 	    // Init Fields form array
 	    $fields_form[0]['form'] = array(
 	        'legend' => array(
@@ -73,7 +87,19 @@ class Ecomail extends Module {
 	                'name' => 'ecomail_api_key',
 	                'size' => 20,
 	                'required' => true
-	            )
+	            ),
+				array(
+				  'type' => 'select',                              // This is a <select> tag.
+				  'label' => $this->l('Vyberte list:'),         // The <label> for this <select> tag.
+				  'desc' => $this->l('Vyberte list do kterého budou zapsáni noví zákazníci'),  // A help text, displayed right next to the <select> tag.
+				  'name' => 'ecomail_list_id',                     // The content of the 'id' attribute of the <select> tag.
+				  'required' => true,                              // If set to true, this option must be set.
+				  'options' => array(
+				    'query' => $options,                           // $options contains the data itself.
+				    'id' => 'id_option',                           // The value of the 'id' key must be the same as the key for 'value' attribute of the <option> tag in each $options sub-array.
+				    'name' => 'name'                               // The value of the 'name' key must be the same as the key for the text content of the <option> tag in each $options sub-array.
+				  )
+				),
 	        ),
 	        'submit' => array(
 	            'title' => $this->l('Save'),
@@ -112,27 +138,63 @@ class Ecomail extends Module {
 	    );
 
 	    // Load current value
-	    $helper->fields_value['ecomail_default_list'] = Configuration::get('ecomail_default_list');
-
+	    $helper->fields_value['ecomail_api_key'] = Configuration::get('ecomail_api_key');
+		$helper->fields_value['ecomail_list_id'] = Configuration::get('ecomail_list_id');
+		
 	    return $helper->generateForm($fields_form);
 	}
  
 	public function hookBackOfficeHeader()
 	{
-		$js .= '<script type="text/javascript" src="/modules/js/scripts.js"></script>';
+		$js .= '<script type="text/javascript" src="'.$this->_path.'	js/scripts.js"></script>';
 		return $js;
 	}
 		
 	public function hookActionCustomerAccountAdd($params)
 	{
 		//$this->hookAuthentication($params);
-		var_dump($params);
+		//var_dump($params);
 		//$params['_POST']['customer_firstname']
 		//$params['_POST']['customer_lastname']
 		//$params['_POST']['email']
 		//$params['_POST']['newsletter']
 		//$params['_POST']['optin']
-		exit();
+		if($params['_POST']['newsletter']){
+			$this->callAPI(Configuration::get('ecomail_api_key'), 'POST', 'subscribers/'.Configuration::get('ecomail_list_id'), array(
+				'email' => $params['_POST']['email'],
+				'name' => $params['_POST']['customer_firstname'].' '.$params['_POST']['customer_lastname']
+			));
+		}
+	}
+	
+	public function callAPI($key, $method, $url, $data = false)
+	{
+	    $curl = curl_init();
+
+	    switch ($method)
+	    {
+	        case "POST":
+	            curl_setopt($curl, CURLOPT_POST, 1);
+
+	            if ($data)
+	                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+	            break;
+	        case "PUT":
+	            curl_setopt($curl, CURLOPT_PUT, 1);
+	            break;
+	        default:
+	            if ($data)
+	                $url = sprintf("%s?%s", $url, http_build_query($data));
+	    }
+
+	    curl_setopt($curl, CURLOPT_URL, 'http://api.ecomailapp.cz/'.$url.'?key='.$key);
+	    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+	    $result = curl_exec($curl);
+
+	    curl_close($curl);
+
+	    return $result;
 	}
 	
 }
